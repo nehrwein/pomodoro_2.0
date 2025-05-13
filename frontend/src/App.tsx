337e4fa01589b6589330219a8d559b2691c3f5df
Birgit <birgit@hey.com>
2025-05-13 20:22:08 +0200

    create frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import Settings from "./pages/Settings"
+import Report from "./pages/Report"
 
 function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   )
 }
 
