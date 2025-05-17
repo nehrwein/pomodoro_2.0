7dc62fafc024659bd48ce57cf2289f784fb0eca6
Birgit <birgit@hey.com>
2025-05-17 12:51:27 +0200

    using new components for login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,131 +1,143 @@
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import { useUserStore } from "@/lib/useUserStore";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+import { useUserStore } from "@/lib/useUserStore"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import api from "@/lib/axios"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [mode, setMode] = useState("auth/signin");
-  const accessToken = useUserStore((state) => state.accessToken);
-  const error = useUserStore((state) => state.error);
-  const setAccessToken = useUserStore((state) => state.setAccessToken);
-  const setUserId = useUserStore((state) => state.setUserId);
-  const setUsernameStore = useUserStore((state) => state.setUsername);
-  const setError = useUserStore((state) => state.setError);
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [mode, setMode] = useState("signin")
+  const accessToken = useUserStore((state) => state.accessToken)
+  const error = useUserStore((state) => state.error)
+  const setAccessToken = useUserStore((state) => state.setAccessToken)
+  const setUserId = useUserStore((state) => state.setUserId)
+  const setUsernameStore = useUserStore((state) => state.setUsername)
+  const setError = useUserStore((state) => state.setError)
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (accessToken) {
-      navigate("/");
+      navigate("/")
     }
-  }, [accessToken, navigate]);
+  }, [accessToken, navigate])
 
   const loginMutation = useMutation({
     mutationFn: async ({
       username,
       password,
     }: {
-      username: string;
-      password: string;
+      username: string
+      password: string
     }) => {
-      const response = await api.post(mode, { username, password });
-      return response.data;
+      const response = await api.post(`auth/${mode}`, { username, password })
+      return response.data
     },
     onSuccess: (data) => {
-      setAccessToken(data.token);
-      setUserId(data.user.id);
-      setUsernameStore(data.user.username);
-      setError(null);
+      setAccessToken(data.token)
+      setUserId(data.user.id)
+      setUsernameStore(data.user.username)
+      setError(null)
     },
     onError: (error: AxiosError) => {
-      setError(error.message || "Login failed");
+      setError(error.message || "Login failed")
     },
-  });
+  })
 
   const onFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    loginMutation.mutate({ username, password });
-  };
+    loginMutation.mutate({ username, password })
+  }
 
   const onSignIn = () => {
-    setMode("auth/signin");
-    setError(null);
-    setUsername("");
-    setPassword("");
-  };
+    setMode("signin")
+    setError(null)
+    setUsername("")
+    setPassword("")
+  }
+
+  const handleClick = () => {
+    if (mode === "signin") {
+      setMode("signup")
+    } else {
+      onSignIn()
+    }
+  }
 
   return (
     <>
       <main className="h-screen w-full flex absolute flex-col items-center justify-center bg-[url('/assets/tomato-background.jpg')] bg-no-repeat bg-center bg-cover">
-        <div className="w-[60%] bg-white p-10 rounded-lg shadow-[0_0_20px_10px_rgba(0,0,0,0.15)] flex flex-col items-center text-[var(--lightRed)] md:w-auto md:text-[20px] md:h3:text-[22px] lg:text-[23px] lg:p-[70px] lg:h3:text-[25px]">
-          <h3 style={{ color: "var(--red)" }}>
-            {mode === "auth/signin"
-              ? "Log in to your account"
-              : "Create new account"}
-          </h3>
-          <form onSubmit={onFormSubmit}>
-            <fieldset className="border-2 border-red-500 rounded-md flex justify-between items-center pr-5 mb-2 mt-1">
-              <legend>Username:</legend>
-              <input
-                className="border border-transparent w-full h-full outline-none text-[20px]"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </fieldset>
-            <fieldset className="border-2 border-red-500 rounded-md flex justify-between items-center pr-5 mb-2 mt-1">
-              <legend>Password:</legend>
-              <input
-                className="border border-transparent w-full h-full outline-none text-[20px]"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </fieldset>
-            {error && (
-              <div>
-                <p className="text-red-500 text-center font-bold">{error}</p>
-              </div>
-            )}
-            <button
-              type="submit"
-              className="bg-red-500 w-[100px] flex justify-center border-none cursor-pointer text-red-100 text-[20px] font-medium p-[10px] my-[20px] mx-auto rounded-md shadow-[0_3px_10px_rgba(0,0,0,0.2)] hover:bg-red-500/75 active:bg-red-500  md:text-[22px] md:w-[120px] lg:text-[25px] lg:w-[150px]"
-            >
-              {mode === "signup" ? "Submit" : "Log in"}
-            </button>
-            <div>
-              {mode === "auth/signin" ? (
-                <div className="flex flex-col justify-center text-center mx-auto border-t border-gorangebg-orange-800 p-2">
-                  <p className="m-1">New to our app? </p>
-                  <p
-                    className="font-bold cursor-pointer underline"
-                    onClick={() => setMode("auth/signup")}
-                  >
-                    Create an account
-                  </p>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Pomodoro App</CardTitle>
+            <CardDescription>
+              {mode === "signin"
+                ? "Log in to your account"
+                : "Create a new account"}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={onFormSubmit}>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Username</Label>
+                  <Input
+                    id="name"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter your username"
+                  />
                 </div>
-              ) : (
-                <div className="flex flex-col justify-center text-center mx-auto border-t border-gray-400 p-2">
-                  <p className="m-1">Already have an account? </p>
-                  <p
-                    className="font-bold cursor-pointer underline"
-                    onClick={onSignIn}
-                  >
-                    Log in
-                  </p>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <Button size="lg" type="submit">
+                  {mode === "signup" ? "Submit" : "Log in"}
+                </Button>
+              </div>
+
+              {error && (
+                <div>
+                  <p className="text-primary text-center font-bold">{error}</p>
                 </div>
               )}
-            </div>
-          </form>
-        </div>
+
+              <div className="flex flex-col justify-center text-center mx-auto border-t ">
+                <p className="m-1">
+                  {mode === "signin"
+                    ? "New to our app?"
+                    : "Already have an account?"}
+                </p>
+                <Button variant="ghost" onClick={handleClick}>
+                  {mode === "signin" ? "Create an account" : "Login"}
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
       </main>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
