6332602d9091aa3abf67729731a9699ad014da82
Birgit <birgit@hey.com>
2025-05-20 20:03:09 +0200

    script for running frontend with or without mocks

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,8 @@ import { BASE_URL } from "./constants"
 import { useUserStore } from "./useUserStore"
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? "" : BASE_URL,
+  baseURL:
+    import.meta.env.DEV && import.meta.env.VITE_DISABLE_MSW ? BASE_URL : "",
 })
 
 api.interceptors.request.use((config) => {
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,15 +7,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 const queryClient = new QueryClient()
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return
+  if (import.meta.env.DEV && !import.meta.env.VITE_DISABLE_MSW) {
+    const { worker } = await import("./mocks/browser.ts")
+
+    // `worker.start()` returns a Promise that resolves
+    // once the Service Worker is up and ready to intercept requests.
+    return worker.start()
   }
-
-  const { worker } = await import("./mocks/browser.ts")
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start()
 }
 
 async function main() {
