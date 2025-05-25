0f291e85e03a2c1197b579c2361432f3a1c4996d
Birgit <birgit@hey.com>
2025-05-25 18:38:46 +0200

    add button controls

--- frontend/src/components/PomodoroButtons.tsx
+++ frontend/src/components/PomodoroButtons.tsx
@@ -1,35 +1,70 @@
+import { Button } from "./ui/button"
+import { Ban, CirclePause, Play, RotateCcw } from "lucide-react"
+
 const PomodoroButtons = () => {
+  const work = false
+  // const buttonBackgroundColor = work ? "darkred" : "darkblue"
+  const activatedButton = true
+  const isRunning = false
+  const iconColor = work ? "red" : "blue"
+
+  const onClickReplay = () => {}
+  const onClickDelete = () => {}
+  const setIsRunning = () => {}
+
   return (
     <>
-      <ButtonsContainer buttonBackgroundColor={buttonBackgroundColor}>
-        <InnerButtonContainer>
-          <TimerButton disabled={!activatedButton} onClick={onClickReplay}>
-            <TimerIcon iconColor={iconColor} active>
+      <div className="flex justify-between gap-1">
+        <Button
+          variant="secondary"
+          disabled={!activatedButton}
+          onClick={onClickReplay}
+        >
+          <RotateCcw
+            color={iconColor}
+            className="text-3xl md:text-4xl lg:text-5xl"
+          />
+          {/* <TimerIcon iconColor={iconColor} active>
+              <
               {ReplayIcon}
-            </TimerIcon>
-          </TimerButton>
-          {isRunning ? (
-            <TimerButton
-              disabled={!activatedButton}
-              onClick={() => dispatch(timer.actions.setisRunning(false))}
-            >
-              <BigIcon iconColor={iconColor}>{PauseIcon}</BigIcon>
-            </TimerButton>
-          ) : (
-            <TimerButton
-              disabled={!activatedButton}
-              onClick={() => dispatch(timer.actions.setisRunning(true))}
-            >
-              <BigIcon iconColor={iconColor}>{PlayIcon}</BigIcon>
-            </TimerButton>
-          )}
-          <TimerButton disabled={!activatedButton} onClick={onClickDelete}>
-            <TimerIcon iconColor={iconColor}>{StopIcon}</TimerIcon>
-          </TimerButton>
-        </InnerButtonContainer>
-      </ButtonsContainer>
+            </TimerIcon> */}
+        </Button>
+        {isRunning ? (
+          <Button
+            variant="secondary"
+            disabled={!activatedButton}
+            onClick={() => setIsRunning()}
+          >
+            <CirclePause
+              color={iconColor}
+              className="text-5xl md:text-6xl lg:text-7xl"
+            />
+          </Button>
+        ) : (
+          <Button
+            variant="secondary"
+            disabled={!activatedButton}
+            onClick={() => setIsRunning()}
+          >
+            <Play
+              color={iconColor}
+              className="text-5xl md:text-6xl lg:text-7xl"
+            />
+          </Button>
+        )}
+        <Button
+          variant="secondary"
+          disabled={!activatedButton}
+          onClick={onClickDelete}
+        >
+          <Ban
+            color={iconColor}
+            className="text-3xl md:text-4xl lg:text-5xl hover:text-white transition ease-in-out duration-500"
+          />
+        </Button>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default PomodoroButtons;
+export default PomodoroButtons
--- frontend/src/components/PomodoroTImer.tsx
+++ frontend/src/components/PomodoroTImer.tsx
@@ -1,35 +1,40 @@
-import { useState } from "react";
+import { useState } from "react"
+import PomodoroButtons from "./PomodoroButtons"
 
 const PomodoroTImer = () => {
-  const [work, setWork] = useState(false);
-  const timerMinutes = 10;
-  const timerSeconds = 35;
-  const description = "Lorem Ipsum";
-  const percentage = 40;
-  const animationColor = "green";
+  const [work, setWork] = useState(true)
+  const timerMinutes = 10
+  const timerSeconds = 35
+  const description = "Lorem Ipsum"
+  const percentage = 0
+  const animationColor = "green"
 
   return (
-    <div
-      className={
-        `relative w-full m-0 py-7 md:py-24 lg:py-12 text-foreground bg-no-repeat bg-center bg-cover ` +
-        (work
-          ? "bg-[url('/assets/timer-work-background.png')]"
-          : "bg-[url('/assets/timer-break-background.png')]")
-      }
-    >
+    <>
       <div
-        className="absolute inset-0"
-        style={{ width: `${percentage}%`, background: `${animationColor}` }}
-      />
-      <div className="flex justify-center">
-        <div className="text-center text-secondary">
-          <h1 className="text-5xl m-0 md:text-6xl lg:text-7xl">
-            {timerMinutes}:{timerSeconds}
-          </h1>
-          <p className="md:text-2xl lg:text-4xl">{description}</p>
+        className={
+          `relative w-full m-0 py-7 md:py-24 lg:py-12 text-foreground bg-no-repeat bg-center bg-cover ` +
+          (work
+            ? "bg-[url('/assets/tomaten_rot.jpg')]"
+            : "bg-[url('/assets/tomaten_gruen.jpg')]")
+        }
+      >
+        <div
+          className="absolute inset-0"
+          style={{ width: `${percentage}%`, background: `${animationColor}` }}
+        />
+        <div className="flex justify-center">
+          <div className="text-center text-secondary">
+            <h1 className="text-5xl m-0 md:text-6xl lg:text-7xl">
+              {timerMinutes}:{timerSeconds}
+            </h1>
+            <p className="md:text-2xl lg:text-4xl">{description}</p>
+          </div>
         </div>
-      </div>
-      {/* <PomodoroButtons
+        <div className="w-full flex justify-center">
+          <PomodoroButtons />
+        </div>
+        {/* <PomodoroButtons
         setSeconds={setSeconds}
         setMinutes={setMinutes}
         setSecondsLeft={setSecondsLeft}
@@ -37,8 +42,9 @@ const PomodoroTImer = () => {
         totalSeconds={totalSeconds}
         work={work}
       /> */}
-    </div>
-  );
-};
+      </div>
+    </>
+  )
+}
 
-export default PomodoroTImer;
+export default PomodoroTImer
