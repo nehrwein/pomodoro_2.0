072935f5e9a02380adf314688425ac5e7c1886c2
Birgit Nehrwein <Birgit.Nehrwein@tunstall.com>
2025-06-03 16:40:46 +0200

    posting new tasks

--- frontend/src/components/AddTask.tsx
+++ frontend/src/components/AddTask.tsx
@@ -1,33 +1,67 @@
+import { useMutation } from "@tanstack/react-query";
 import { Plus } from "lucide-react";
 import { useState } from "react";
+import api from "@/lib/axios";
+import { useUserStore } from "@/lib/useUserStore";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
   const taskColor = "blue";
+  const userId = useUserStore().userId;
+
+  const postTask = async ({
+    description,
+    userId,
+  }: {
+    description: string;
+    userId: string;
+  }) => {
+    const response = await api.post("/tasks", {
+      description,
+      user: userId,
+    });
+    return response.data;
+  };
+
+  const useAddTask = () =>
+    useMutation({
+      mutationFn: postTask,
+    });
+
+  //TODO - handle loading and error
+  const { mutate: addTask, isPending, isError } = useAddTask();
+
+  const handleAddTask = () => {
+    if (userId) {
+      addTask({ description: task, userId });
+      setTask("");
+    }
+  };
 
   return (
     <div
       className={`w-full flex items-center justify-center border-t-2 gap-2.5 py-5`}
       style={{ borderTopColor: taskColor }}
     >
-      <Input
-        // taskColor={taskColor}
-        id="task"
-        type="text"
-        value={task}
-        placeholder="Add new task"
-        onChange={(e) => setTask(e.target.value)}
-        // onKeyPress={onPressEnter}
-      />
-      <Button
-        type="submit"
-        disabled={!task}
-        // onClick={() => onAddTodo(accessToken, userId, task)}
-      >
-        <Plus />
-      </Button>
+      <div className="flex w-2xl gap-2">
+        <Input
+          id="task"
+          type="text"
+          value={task}
+          placeholder="Add new task"
+          onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleAddTask();
+            }
+          }}
+        />
+        <Button type="submit" disabled={!task} onClick={() => handleAddTask()}>
+          <Plus />
+        </Button>
+      </div>
     </div>
   );
 };
